0407ae56dedb6f32bbf45714cc2071cc91064493
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-01-05 22:15:24 +0100

    proj setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return <div className="App"></div>;
+}
+
+/*Advanced React Hooks https://youtu.be/324czHa9O9U?t=3563 */
+
+/*Advanced React Patterns:  https://www.youtube.com/watch?v=MfIoAG3e7p4&ab_channel=codedamn */
+
+export default App;
